86172dc76d1ddca0406e1ceed0fe6e2e710a7360
Kendrick Winata <kendrickwinata@Najwas-MacBook-Pro-2.local>
2022-09-03 16:42:03 +0800

    chore: commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import React from "react";
 import { ToastContainer } from "react-toastify";
+import SiteHeader from './components/partials/SiteHeader';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+  
+  <div>
+
+    <SiteHeader/>
+    <div>hello world</div>
+  </div>
+
+  
+  )
 }
 
 export default App;
