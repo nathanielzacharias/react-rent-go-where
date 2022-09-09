1422d296477f43530ad44765126a487926a1a956
nathanielzacharias <nathanielzacharias@gmail.com>
2022-09-09 12:23:23 +0800

    feat: added first route path for /. It works with BE express

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import React from "react";
 import { ToastContainer } from "react-toastify";
-import SiteHeader from './components/partials/SiteHeader';
-import Sidebar from './components/partials/Sidebar';
-import PropertyGrid from './components/property/PropertyGrid'
-import PropertyCard from './components/property/PropertyCard'
-import Homepage from './components/homepage-container/Homepage';
+import SiteHeader from "./components/partials/SiteHeader";
+import Sidebar from "./components/partials/Sidebar";
+import PropertyGrid from "./components/property/PropertyGrid";
+import PropertyCard from "./components/property/PropertyCard";
+import Homepage from "./components/homepage-container/Homepage";
 
-
-import { Button } from 'antd';
-import { Pagination } from 'antd';
+import { Button } from "antd";
+import { Pagination } from "antd";
 
 import "./antd/dist/antd.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -19,15 +18,15 @@ import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-  
-  <div>
+    <div>
+      <SiteHeader />
 
-    <SiteHeader/>
-    <Homepage/>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
 
-  </div>
-  
-  )
+    </div>
+  );
 }
 
 export default App;
