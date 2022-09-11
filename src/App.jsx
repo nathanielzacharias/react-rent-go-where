ce6cf9293fdb2365e315c0387dee523929986e1c
Kendrick Winata <kendrickwinata@Najwas-MacBook-Pro-2.local>
2022-09-12 00:42:07 +0800

    chore: tidy up listing page

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ function App() {
         <Route path="/api/v1/auth/login" element={<Guest component={Login} />} />
         <Route path ="/api/v1/profile" element={<Auth component={userProfile} />} />
         <Route path="/api/v1/auth/register" element={<SignUp />} />
+        <Route path="/user/:userID" element={<Homepage />} />
       </Routes>
 
     </div>
