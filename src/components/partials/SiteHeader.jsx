460f8b286cdf38d5ec6bb398a1a13ecd224ba037
Yui Xiong Ng <jadebear15@gmail.com>
2022-09-11 17:16:59 +0800

    chore: fix navbar register+login links

--- src/components/partials/SiteHeader.jsx
+++ src/components/partials/SiteHeader.jsx
@@ -1,38 +1,64 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function SiteHeader() {
+  const token = localStorage.getItem("user_token");
 
-    return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Rent-Go-Where</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Favourite</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Potential Housemate</a>
-              </li>
-            
-            </ul>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Rent-Go-Where
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Favourite
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Potential Housemate
+              </a>
+            </li>
+          </ul>
 
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Register</a>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+            {
+            token ? (
+              <li className="nav-link">
+                <Link to="/api/v1/profile">Profile</Link>
               </li>
-              <li className="nav-item my-2 my-lg-0">
-                <a className="nav-link active mr-sm-2" aria-current="page" href="#">Login</a>
-              </li>
-            </ul>
-            
-          </div>
+
+            ) : (
+              <>
+                <li className="nav-link">
+                  <Link to="/api/v1/auth/register">Register</Link>
+                </li>
+                <li className="nav-link">
+                  <Link to="/api/v1/auth/login">Login</Link>
+                </li>
+              </>
+              )
+            }
+
+          </ul>
         </div>
-      </nav>
-    )
+      </div>
+    </nav>
+  );
 }
 
-export default SiteHeader
+export default SiteHeader;
