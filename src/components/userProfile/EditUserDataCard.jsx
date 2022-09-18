c2ab711aa7af72a827cb482fc1354dba7465cdc4
Yui Xiong Ng <jadebear15@gmail.com>
2022-09-18 22:15:25 +0800

    chore: still trying out form input

--- src/components/userProfile/EditUserDataCard.jsx
+++ src/components/userProfile/EditUserDataCard.jsx
@@ -1,86 +1,95 @@
 import React, { useEffect, useState } from "react";
-import {Form, Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import { toast } from "react-toastify";
 import { useParams, useNavigate } from "react-router-dom";
 
-import DeleteUserComponent from './DeleteUserComponent'
+import DeleteUserComponent from "./DeleteUserComponent";
 
 function EditUserDataCard(props) {
-  console.log ('props.data is', props.data)
-  const { _id, name, email, budget, location, ethinicity, nationality, gender, profilePic} = props.data
- // console.log(email)
-  // console.log(gender)
-  // const navigate = useNavigate();
 
-  // const [formData, setFormData] = useState();
+  const {
+    _id,
+    name,
+    email,
+    budget,
+    location,
+    ethinicity,
+    nationality,
+    gender,
+    profilePic,
+  } = props.data;
 
-  // function handleInputChange(e) {
-  //   setFormData({
-  //     ...formData,
-  //     [e.target.name]: e.target.value,
-  //   });
+  const navigate = useNavigate();
+  const [inputGender, setInputGender] = useState()
+  const [user, setUser] = useState(null);
+  const [formData, setFormData] = useState({
+    userId: `${_id}`,
+    gender: inputGender
+  });
 
-  //   const handleFormSubmit = (event) => {
-  //     event.preventDefault();
-  
-  //     fetch(`http://localhost:8000/api/v1/app/profile/${_id}`, {
-  //       method: "POST",
-  //       body: JSON.stringify(formData),
-  //       headers: {
-  //         "Content-type": "application/json",
-  //       },
-  //     })
-  //       .then((jsonResponse) => {
-  //         // displaying success message
-  //         toast.success("Create successful");
-  //         console.log("edit user details successful");
-  
-  //         // redirect to property listing page
-  //         navigate("/");
-  //       })
-  //       .catch((err) => {
-  //         toast.error(err.message);
-  //       });
-  //   };
+  setFormData(props.data)
+  console.log("formdata is :", formData)
+  // console.log("props.data is", props.data);
+
+  function handleInputChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+      userId : _id,
+    });
+  }
+  console.log('stringify:', JSON.stringify(formData));
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(`http://localhost:8000/api/v1/profile/update/`, {
+      method: "PATCH",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((jsonResponse) => {
+        // displaying success message
+        toast.success("Edit User successful");
+        console.log("Edit User successful");
+
+        // redirect to property listing page
+        // navigate("/");
+      })
+      .catch((err) => {
+        toast.error(err.message);
+      });
+  };
 
   return (
     <div>
-      <Form  style={{ margin: "10px 400px" }}>
-      <Form.Group className="mb-3">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="text"
-          name="Email"
-          value={email}
-          // onChange={handleInputChange}
-          placeholder= "Email"
-        />
-      </Form.Group>
-      
-      <Form.Group className="mb-3">
-        <Form.Label>Gender</Form.Label>
-        <Form.Control
-          type="text"
-          name="Gender"
-          value={gender}
-          // onChange={handleInputChange}
-          placeholder= "Gender"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>location</Form.Label>
-        <Form.Control
-          type="text"
-          name="location"
-          value={location}
-          // onChange={handleInputChange}
-          placeholder= "location"
-        />
-      </Form.Group>
-      <DeleteUserComponent userObjId={_id}/>
-   </Form>
+      <Form onSubmit={handleFormSubmit} style={{ margin: "10px 400px" }}>
+
+      <label htmlFor="gender" className="form-label">
+                gender
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="gender"
+                value= {formData.gender}
+                name="gender"
+                onChange={(e) => handleInputChange(e.target.value)}
+              />
+
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        
+        <DeleteUserComponent userObjId={_id} />
+
+        
+      </Form>
     </div>
-  )
+  );
 }
 
-export default EditUserDataCard
+export default EditUserDataCard;
