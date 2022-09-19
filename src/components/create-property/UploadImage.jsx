49b580303714d93eded32b10022c9bfff086c036
Kendrick Winata <kendrickwinata@outlook.com>
2022-09-20 02:24:15 +0800

    feat: added delete property

--- src/components/create-property/UploadImage.jsx
+++ src/components/create-property/UploadImage.jsx
@@ -1,8 +1,6 @@
-
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import Button from "react-bootstrap/Button";
-
 
 import { IKContext, IKImage, IKUpload } from "imagekitio-react";
 
@@ -12,7 +10,6 @@ const urlEndpoint = "https://ik.imagekit.io/vsoncvhkm/";
 // optional parameters (needed for client-side upload)
 const publicKey = "public_VW2hCnJrSlQCWKZA9Xk7NnP3oxo=";
 const authenticationEndpoint = "http://localhost:8000/auth";
-
 
 function ImageUpload({ propertyImages, setPropertyImages }) {
   const [imagePath, setImagePath] = useState([]);
@@ -53,34 +50,18 @@ function ImageUpload({ propertyImages, setPropertyImages }) {
         textAlign: "left",
         margin: "0px 0px 15px 0px",
       }}
-
-const onError = (err) => {
-  console.log("Error", err);
-};
-
-const onSuccess = (res) => {
-  console.log("Success", res);
-};
-
-function ImageUpload() {
-  return (
-    <div
-      className="App"
-      style={{ textAlign: "left", margin: "0px 0px 15px 0px" }}>
+    >
       <IKContext
         publicKey={publicKey}
         urlEndpoint={urlEndpoint}
         authenticationEndpoint={authenticationEndpoint}
       >
-
         <p>Upload image</p>
-
 
         <IKUpload
           fileName="test-upload.png"
           onError={onError}
           onSuccess={onSuccess}
-
           style={{
             textAlign: "left",
             margin: "0px 0px 15px 0px",
@@ -103,7 +84,6 @@ function ImageUpload() {
           />
         ))}
         {/* <IKImage path={imagePath[0]} /> */}
-
       </IKContext>
       {/* ...other SDK components added previously */}
     </div>
